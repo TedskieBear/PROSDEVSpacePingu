6e2452ba06a890853e347395a1055a2946be5d7b
tokiiiiiiiin <tokiinyan@gmail.com>
2018-08-05 23:59:27 +0800

    Obstacle collission
    Kulang pa spawner

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Bird : MonoBehaviour
 {
     // Movement speed
-    public float speed = 2;
+    // public float speed = 2;
 
     // Flap force
     public float force = 300;
@@ -13,7 +13,8 @@ public class Bird : MonoBehaviour
     void Start()
     {
         // Fly towards the right
-        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        // GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        // Pingu used to move, now we'll make it work so that Pingu only moves up and down, but the obstacles are moving -- perceived movement is still the same
     }
 
     // Update is called once per frame
@@ -21,12 +22,27 @@ public class Bird : MonoBehaviour
     {
         // Flap
         if (Input.GetKeyDown(KeyCode.Space))
+
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
     }
 
     void OnCollisionEnter2D(Collision2D coll)
     {
         // Restart
-        Application.LoadLevel(Application.loadedLevel);
+        if (coll.gameObject.name == "ground")
+        {
+            Application.LoadLevel(Application.loadedLevel); // this should be changed to a game over state :)
+        }
+        else if (coll.gameObject.name == "Diamond Reward")
+        {
+            Destroy(coll.gameObject); // destroy the diamond object (make it disappear)
+            // add code to add points to player's score
+            // diamonds are for currency, added to player's amount at the end of the game proper
+        }
+        else if (coll.gameObject.name == "Choco Butternut Donut" || coll.gameObject.name == "Strawberry Donut" || coll.gameObject.name == "Bavarian Donut")
+        {
+            Debug.Log("Game Over");
+            Application.LoadLevel(Application.loadedLevel);
+        }
     }
 }

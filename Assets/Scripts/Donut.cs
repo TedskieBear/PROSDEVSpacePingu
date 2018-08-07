441af8d91e47577e4a859854a2564dea9b86c712
carlacresencio <carla_cresencio@dlsu.edu.ph>
2018-08-07 18:52:33 +0800

    collision fixes and diamond count
    pingu doesnt die anymore when he gets a diamond
    the diamond doesnt collide with the donuts anymore

--- Assets/Scripts/Donut.cs
+++ Assets/Scripts/Donut.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Donut : MonoBehaviour {
 
     // Move speed
     public float speed = 5;
+    public Text diamond;
 
     // Use this for initialization
     void Start () {
@@ -14,12 +16,26 @@ public class Donut : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+    
+    }
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        Destroy(col.gameObject);
 
+        if (col.gameObject.tag == "Player"){
+            if (this.gameObject.tag == "Diamond"){
+                diamond.GetComponentInParent<DiamondScript> ().add();
+		        Destroy(this);
+            }
+            else if (this.gameObject.tag != "Diamond")
+                Destroy(col.gameObject);
+        }
+        else{
+            Physics2D.IgnoreLayerCollision(8, 9, true);
+            
+            Physics2D.IgnoreLayerCollision(8, 9, true);
+        }
+            // Physics2D.IgnoreCollision(col.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+            
     }
 }
